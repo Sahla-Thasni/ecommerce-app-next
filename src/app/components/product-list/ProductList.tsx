ec0421721cde1801de35923a8f4082d3e57a2cd4
Sahla Thasni <sahlathasni.p93@gmail.com>
2026-06-14 01:41:10 +0400

    Test productlist page

--- src/app/components/product-list/ProductList.tsx
+++ src/app/components/product-list/ProductList.tsx
@@ -1,20 +1,25 @@
-import ProductCard from '../product-card/ProductCard';
-import { ProductService } from '@/app/services/product-service';
+import ProductCard from "../product-card/ProductCard";
+import { ProductService } from "@/app/services/product-service";
 
+// export default async function ProductList() {
+//   const products = await ProductService.getproducts();
 
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         {products.map((p: any) => (
+//           <div key={p.id} className="col-md-4 mb-4">
+//             <ProductCard product={p} />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 export default async function ProductList() {
-  const products = await ProductService.getproducts();
-
   return (
-    <div className="container">
-      <div className="row">
-        {products.map((p: any) => (
-          <div key={p.id} className="col-md-4 mb-4">
-            <ProductCard product={p} />
-          </div>
-        ))}
-      </div>
+    <div>
+      <h1>Products Page Works</h1>
     </div>
   );
 }
-
