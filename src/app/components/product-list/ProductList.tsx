71a3607879345650581ce2208dd2f7bd9b0d31cb
Sahla Thasni <sahlathasni.p93@gmail.com>
2026-06-14 02:06:53 +0400

    Move products fetch to client side"

--- src/app/components/product-list/ProductList.tsx
+++ src/app/components/product-list/ProductList.tsx
@@ -1,4 +1,4 @@
-// import ProductCard from "../product-card/ProductCard";
+ import ProductCard from "../product-card/ProductCard";
 // import { ProductService } from "@/app/services/product-service";
 
 // export default async function ProductList() {
@@ -16,31 +16,71 @@
 //     </div>
 //   );
 // }
-import ProductCard from "../product-card/ProductCard";
+// import ProductCard from "../product-card/ProductCard";
+// import { ProductService } from "@/app/services/product-service";
+
+// export default async function ProductList() {
+//   try {
+//     const products = await ProductService.getproducts();
+
+//     return (
+//       <div className="container">
+//         <div className="row">
+//           {products.map((p: any) => (
+//             <div key={p.id} className="col-md-4 mb-4">
+//               <ProductCard product={p} />
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   } catch (error) {
+//     console.error(error);
+
+//     return (
+//       <div className="container">
+//         <h2>Error loading products</h2>
+//       </div>
+//     );
+//   }
+// }
+
+
+"use client";
+
+import { useEffect, useState } from "react";
 import { ProductService } from "@/app/services/product-service";
 
-export default async function ProductList() {
-  try {
-    const products = await ProductService.getproducts();
+export default function ProductList() {
+  const [products, setProducts] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
-    return (
-      <div className="container">
-        <div className="row">
-          {products.map((p: any) => (
-            <div key={p.id} className="col-md-4 mb-4">
-              <ProductCard product={p} />
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  } catch (error) {
-    console.error(error);
+  useEffect(() => {
+    ProductService.getproducts()
+      .then((data) => {
+        setProducts(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
 
-    return (
-      <div className="container">
-        <h2>Error loading products</h2>
-      </div>
-    );
+  if (loading) {
+    return <h2>Loading...</h2>;
   }
+
+
+  return (
+    <div className="container">
+      <div className="row">
+        {products.map((p: any) => (
+          <div key={p.id} className="col-md-4 mb-4">
+            <ProductCard product={p} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
